5b02ea8a0ecb5c7e04dc086a0f49a32dba09b657
LisDem <lis.demlord@gmail.com>
2023-06-02 06:58:24 +0500

    Mod Base and Settings fragments

--- app/src/main/java/com/example/messengerktv2/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/messengerktv2/fragments/BaseFragment.kt
@@ -1,7 +1,17 @@
 package com.example.messengerktv2.fragments
 
 import androidx.fragment.app.Fragment
+import com.example.messengerktv2.MainActivity
 
-class BaseFragment : Fragment() {
+open class BaseFragment(layout: Int) : Fragment(layout) {
 
+    override fun onStart() {
+        super.onStart()
+        (activity as MainActivity).mAppDrawer.disableDrawer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as MainActivity).mAppDrawer.enableDrawer()
+    }
 }
--- app/src/main/java/com/example/messengerktv2/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/messengerktv2/fragments/SettingsFragment.kt
@@ -1,6 +1,43 @@
 package com.example.messengerktv2.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.messengerktv2.R
+import com.example.messengerktv2.databinding.FragmentSettingsBinding
+import com.example.messengerktv2.utilities.DataSaveHelper
+import com.example.messengerktv2.utilities.replaceFragment
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+    private val binding by viewBinding(FragmentSettingsBinding::bind)
+    private val loginData: DataSaveHelper by activityViewModels()
+
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        loginData.login.observe(activity as LifecycleOwner) {
+            binding.settingsFullName.text = it
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_menu_exit -> {
+                replaceFragment(LoginFragment())
+            }
+        }
+        return true
+    }
 }
