e107072dacbcad069e87459ab19c61ccdc14805b
LisDem <lis.demlord@gmail.com>
2023-06-02 06:36:27 +0500

    Mod LoginFragment and MainActivity

--- app/src/main/java/com/example/messengerktv2/MainActivity.kt
+++ app/src/main/java/com/example/messengerktv2/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.messengerktv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.example.messengerktv2.databinding.ActivityMainBinding
+import com.example.messengerktv2.fragments.LoginFragment
+import com.example.messengerktv2.`object`.AppDrawer
+import com.example.messengerktv2.utilities.replaceFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    lateinit var mAppDrawer: AppDrawer
+    private lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        mAppDrawer.create()
+        replaceFragment(LoginFragment(), false)
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+        mAppDrawer = AppDrawer(this, mToolbar)
     }
 }
--- app/src/main/java/com/example/messengerktv2/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/messengerktv2/fragments/LoginFragment.kt
@@ -1,6 +1,29 @@
 package com.example.messengerktv2.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.messengerktv2.R
+import com.example.messengerktv2.databinding.FragmentLoginBinding
+import com.example.messengerktv2.utilities.DataSaveHelper
+import com.example.messengerktv2.utilities.replaceFragment
+import com.example.messengerktv2.utilities.showToast
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private val binding by viewBinding(FragmentLoginBinding::bind)
+    private val dataBase: DataSaveHelper by activityViewModels()
+
+    override fun onStart() {
+        super.onStart()
+        binding.registerBtnNext.setOnClickListener { checkLogin() }
+    }
+
+    private fun checkLogin() {
+        if (binding.registerInputCode.text.toString().isEmpty()) {
+            showToast("Заполните поле")
+        } else {
+            dataBase.login.value = binding.registerInputCode.text.toString()
+            replaceFragment(ChatsFragment())
+        }
+    }
 }
